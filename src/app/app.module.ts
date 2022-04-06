3c9274df54d22945f98da7e50789fd769c66871e
vickaib <victoriakaibigan@gmail.com>
2022-04-06 00:15:30 -0400

    restructure app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 //Components
 import { AppComponent } from './app.component';
-import { LeftNavbarComponent } from './component/left-navbar/left-navbar.component';
-import { HeaderComponent } from './component/header/header.component';
-
-import { TimesheetComponent } from './component/timesheet/timesheet.component';
+import { LeftNavbarComponent } from './left-navbar/left-navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
 
 @NgModule({
   declarations: [
