1a5c9fa72f09fde84b1db17ea8c1eb5ffdd87f1f
vickaib <victoriakaibigan@gmail.com>
2022-04-06 02:06:41 -0400

    add action section

--- src/app/timesheet/timesheet.component.ts
+++ src/app/timesheet/timesheet.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesheetComponent implements OnInit {
 
+  public dataList: Array<IEmployee> = [
+    {
+      employeeId: 101,
+      employeeFname: 'Mark', 
+      employeeLname: 'Otto',
+      employeePhone: '123-456-7890',
+      employeeEmail: 'mark@dns.com',
+      employeeAddress: '4400 University Dr, Fairfax VA 22030' 
+    },
+
+    {
+      employeeId: 102,
+      employeeFname: 'Jacob', 
+      employeeLname: 'Thorton',
+      employeePhone: '123-456-7890',
+      employeeEmail: 'jacob@dns.com',
+      employeeAddress: '4400 University Dr, Fairfax VA 22030' 
+    },
+
+    {
+      employeeId: 103,
+      employeeFname: 'Jennifer', 
+      employeeLname: 'Brown',
+      employeePhone: '123-456-7890',
+      employeeEmail: 'sam@dns.com',
+      employeeAddress: '4400 University Dr, Fairfax VA 22030' 
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface IEmployee {
+  employeeId: number;
+  employeeFname: string;
+  employeeLname: string;
+  employeePhone: string;
+  employeeEmail: string;
+  employeeAddress: string;
 }
