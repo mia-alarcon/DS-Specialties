a77354072596f417e7faad2d77f2c947c0aa452e
vickaib <victoriakaibigan@gmail.com>
2022-04-05 23:16:27 -0400

    create form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LeftNavbarComponent } from './component/left-navbar/left-navbar.component';
 import { HeaderComponent } from './component/header/header.component';
-
 import { TimesheetComponent } from './component/timesheet/timesheet.component';
-
+import { FormComponent } from './form/form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './form/login/login.component';
+import { SubmissionComponent } from './form/submission/submission.component';
 @NgModule({
   declarations: [
     AppComponent,
     LeftNavbarComponent,
     HeaderComponent,
     TimesheetComponent,
+    FormComponent,
+    LoginComponent,
+    SubmissionComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
